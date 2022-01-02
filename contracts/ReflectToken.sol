834394c5eaec5101991d1b301c04412b44bedf92
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-01-02 02:07:58 +0100

    tweaking

--- contracts/ReflectToken.sol
+++ contracts/ReflectToken.sol
@@ -4,7 +4,30 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ReflectToken is ERC20 {
+
+    mapping(address => bool) private _isExcluded;
+
+    uint256 private constant pointMultiplier = 10 ** 18;
+
+    struct Account {
+        uint balance;
+        uint lastDividendPoints;
+    }
+
+    mapping(address => Account) public accounts;
+
+    uint private totalDividendPoints;
+
+    uint private unclaimedDividends;
+
+    uint private reflectionFee;
+
+    uint private blackListAmount;
+
+    mapping(address => bool) private isBlackListed;
+
     constructor() ERC20("ReflectToken", "RFT") {
+        _isExcluded[msg.sender] = true;
         _mint(msg.sender, 1000000000000);
     }
 }
