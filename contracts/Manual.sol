e905d4074bf91f4981c44b46122a6c575f9e445a
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-01-02 04:01:36 +0100

    good night

--- contracts/Manual.sol
+++ contracts/Manual.sol
@@ -106,6 +106,12 @@ contract Manual {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
         require(_value <= accounts[_from].balance);
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+
+        _transfer(_from, _to, _value); 
+
+        return true;
     }
 
 
